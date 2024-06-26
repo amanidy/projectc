fb0d0a99cd1d826d899ab90f87fe3db77b9cf5dc
Amani <amaniarnold08@gmail.com>
2024-06-26 15:49:38 +0300

    Styled Navbar

--- agri/src/Components/Navbar/Navbar.jsx
+++ agri/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Navbar.css'
-import logo from '../../assets/agrisense-high-resolution-logo-black-transparent.png'
 import { Link } from 'react-router-dom';
   const Navbar = () => {
   return (
 <div>
       <div className='nav'>
-        <div className='nav-logo'><img className='logo' src={ logo} alt=''/></div>
+        <h2>AGRISENSE</h2>
+        
       <ul className='nav-menu'>
         <Link to="/agrisense" className='nav-contact'>Home</Link>
         <Link to="/login" className='nav-contact'>Login</Link>
